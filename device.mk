daef1853450363644325277cd76c78ac409c7ae5
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-08-13 15:33:04 +0200

    ishtar: Switch to inline kernel_platform kernel building
    Change-Id: I574add9e78b2f792aec6e6deae735408ebe5fce1

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@ $(call inherit-product, vendor/xiaomi/ishtar/ishtar-vendor.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Kernel Binary
-KERNEL_PREBUILT_DIR := device/xiaomi/ishtar-kernel
-
 # NFC
 TARGET_USES_ST_NFC := true
 
